d2e783b01dcf3783b83681ad3114967fcda7380c
ZJohnson3131! <johzy002@mymail.unisa.edu.au>
2025-07-10 20:51:19 +0930

    1. two-sum.py submission

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -6,7 +6,11 @@
 
 # @lc code=start
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        for i in range(len(nums)):
+            if (target - nums[i]) in nums and nums.index(target-nums[i]) != i:
+                return [i, nums.index(target-nums[i])]
+
 # @lc code=end
 
+print(Solution().twoSum([3,2,4],6))
